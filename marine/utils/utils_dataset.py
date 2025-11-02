f1db1acb6f848018ec2ffde339a2cbec211e8c08
bu1th4nh <thanh10973@gmail.com>
2025-11-01 23:47:28 -0400

    [Add InstructBLIP support] Debug image

--- marine/utils/utils_dataset.py
+++ marine/utils/utils_dataset.py
@@ -124,6 +124,11 @@ class Collator:
         global_input_images = [x["image"] for x in batch]
         input_prompts = [x["full_prompt"] for x in batch]
         guidance_prompts = [x["full_prompt_neg"] for x in batch]
+
+
+        for img in global_input_images:
+            logging.fatal(f"Image size: {img.size}, mode: {img.mode}")
+
         
         inputs = self.processor(
             images=global_input_images, 
@@ -150,93 +155,3 @@ class Collator:
         return batch
 
 
-
-def custom_collate_fn(batch: List[Tuple[
-    str,          # cur_prompt
-    str,          # question_id
-    str,          # img_id
-    Any,          # inputs
-    Any           # guidance_inputs
-]]) -> Tuple[torch.Tensor, ...]:
-    """
-    Custom collate function to pad input/guidance_ids and attention masks,
-    and stack image tensors. All outputs are moved to CUDA.
-    """
-    prompts = [item[0] for item in batch]
-    question_ids = [item[1] for item in batch]
-    img_ids = [item[2] for item in batch]
-    inputs_list = [item[3] for item in batch]
-    guidance_inputs_list = [item[4] for item in batch]
-
-
-    # logging.fatal(f"Type of prompts: {type(prompts)}")
-    # logging.fatal(f"Type of question_ids: {type(question_ids)}")
-    # logging.fatal(f"Type of img_ids: {type(img_ids)}")
-    # logging.fatal(f"Type of inputs_list: {type(inputs_list)}")
-    # logging.fatal(f"Type of inputs_list[0]: {type(inputs_list[0])}")
-
-
-    logging.fatal(f"input keys: {inputs_list[0]}")
-
-
-    
-    input_ids_list = [inp["input_ids"] for inp in inputs_list]
-    image_tensors = [inp["pixel_values"].squeeze(0) for inp in inputs_list]
-    attention_masks_list = [inp["attention_mask"] for inp in inputs_list]
-
-
-    guidance_ids_list = [g_inp["input_ids"] for g_inp in guidance_inputs_list]  
-    guidance_image_tensors = [g_inp["pixel_values"].squeeze(0) for g_inp in guidance_inputs_list]
-    guidance_attention_masks_list = [g_inp["attention_mask"] for g_inp in guidance_inputs_list]
-
-
-    def process_sequence(seq_list):
-        seq_list = [seq.squeeze(0).flip(dims=[0]) for seq in seq_list]
-        return pad_sequence(seq_list, batch_first=True, padding_value=0).flip(dims=[1])
-
-    input_ids_batch = process_sequence(input_ids_list).cuda()
-    attn_mask_batch = process_sequence(attention_masks_list).cuda()
-    image_tensor_batch = torch.stack(image_tensors).squeeze(1).cuda()
-    
-    guidance_ids_batch = process_sequence(guidance_ids_list).cuda()
-    guidance_attn_mask_batch = process_sequence(guidance_attention_masks_list).cuda()
-    guidance_image_tensor_batch = torch.stack(guidance_image_tensors).squeeze(1).cuda()
-
-
-
-    rtn_values = {
-        "prompts": list(prompts),
-        "question_ids": list(question_ids),
-        "img_ids": list(img_ids),
-        "inputs": {
-            "input_ids": input_ids_batch,
-            "attention_masks": attn_mask_batch,
-            "pixel_values": image_tensor_batch,
-        },
-        "guidance_inputs": {
-            "input_ids": guidance_ids_batch,
-            "attention_masks": guidance_attn_mask_batch,
-            "pixel_values": guidance_image_tensor_batch,
-        }
-    }
-
-
-    if 'qformer_input_ids' in inputs_list[0]:
-        qformer_input_ids_list = [inp['qformer_input_ids'] for inp in inputs_list]
-        qformer_attention_masks_list = [inp['qformer_attention_mask'] for inp in inputs_list]
-        qformer_input_ids_batch = process_sequence(qformer_input_ids_list).cuda()
-        qformer_attn_mask_batch = process_sequence(qformer_attention_masks_list).cuda()
-
-        rtn_values['inputs']['qformer_input_ids'] = qformer_input_ids_batch
-        rtn_values['inputs']['qformer_attention_masks'] = qformer_attn_mask_batch
-
-    if 'qformer_input_ids' in guidance_inputs_list[0]:
-        guidance_qformer_input_ids_list = [inp['qformer_input_ids'] for inp in guidance_inputs_list]
-        guidance_qformer_attention_masks_list = [inp['qformer_attention_mask'] for inp in guidance_inputs_list]
-        guidance_qformer_input_ids_batch = process_sequence(guidance_qformer_input_ids_list).cuda()
-        guidance_qformer_attn_mask_batch = process_sequence(guidance_qformer_attention_masks_list).cuda()
-
-        rtn_values['guidance_inputs']['qformer_input_ids'] = guidance_qformer_input_ids_batch
-        rtn_values['guidance_inputs']['qformer_attention_masks'] = guidance_qformer_attn_mask_batch
-
-    return rtn_values
