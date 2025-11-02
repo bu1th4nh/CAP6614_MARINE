795d883224564a5679851208d69de04dd4ae0074
bu1th4nh <thanh10973@gmail.com>
2025-11-02 01:32:46 -0400

    [Add InstructBLIP support] Update  logit

--- marine/generate_instructblip.py
+++ marine/generate_instructblip.py
@@ -82,7 +82,8 @@ def eval_model(args):
                 output_ids = model.generate(
                     **inputs,
                     do_sample=False,
-                    num_beams=5,
+                    num_beams=1,
+                    num_return_sequences=1,
                     max_length=256,
                     min_length=1,
                     top_p=0.9,
@@ -94,9 +95,10 @@ def eval_model(args):
                 output_ids = model.generate(
                     **inputs,
                     do_sample=False,
-                    num_beams=5,
+                    num_beams=1,
                     max_length=256,
                     min_length=1,
+                    num_return_sequences=1,
                     top_p=0.9,
                     repetition_penalty=1.5,
                     length_penalty=1.0,
