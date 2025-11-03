8e69c6ba424666fa7cdfc8798cae1bcb80ff6059
bu1th4nh <thanh10973@gmail.com>
2025-11-03 18:56:43 -0500

    Update BLIP script

--- scripts/eval_instructBLIP.sh
+++ scripts/eval_instructBLIP.sh
@@ -14,36 +14,36 @@ set +a
 
 
 guidance_strength_lst=(0.0 0.7)
-# benchmark_lst=(chair)
+benchmark_lst=(chair pope)
 
-# for BENCHMARK in "${benchmark_lst[@]}"; do
-#     # Initialize QUESTION_FILE_ls based on BENCHMARK
-#     if [ $BENCHMARK == "chair" ]; then
-#         QUESTION_FILE_ls=(chair_coco_detr_th0.95_ram_th0.68.json)
-#     elif [ $BENCHMARK == "pope" ]; then
-#         QUESTION_FILE_ls=(pope_coco_detr_th0.95_ram_th0.68.json)
-#     fi
+for BENCHMARK in "${benchmark_lst[@]}"; do
+    # Initialize QUESTION_FILE_ls based on BENCHMARK
+    if [ $BENCHMARK == "chair" ]; then
+        QUESTION_FILE_ls=(chair_coco_detr_th0.95_ram_th0.68.json)
+    elif [ $BENCHMARK == "pope" ]; then
+        QUESTION_FILE_ls=(pope_coco_detr_th0.95_ram_th0.68.json)
+    fi
 
-#     OUTPUT_DIR=./output/${MODEL_NAME}/answers/answer_${TYPE}_${BENCHMARK}
+    OUTPUT_DIR=./output/${MODEL_NAME}/answers/answer_${TYPE}_${BENCHMARK}
 
-#     #### Generate answers ####
-#     for guidance_strength in "${guidance_strength_lst[@]}"; do
-#         for QUESTION_FILE in "${QUESTION_FILE_ls[@]}"; do
-#             echo "Running $MODEL_VERSION inference with guidance_strength = $guidance_strength, seed = $SEED, batch_size = $BATCH_SIZE"
-#             python ./marine/generate_${MODEL_NAME}.py \
-#                 --question_file $QUESTION_FILE \
-#                 --guidance_strength $guidance_strength \
-#                 --answer_path $OUTPUT_DIR \
-#                 --model_path $MODEL_VERSION \
-#                 --seed $SEED \
-#                 --batch_size $BATCH_SIZE \
-#                 --image_folder /home/ti514716/Data/coco/val2014 \
-#                 --temperature 0.6 \
-#                 --top_p 0.9 \
-#                 --max_new_tokens 64
-#         done
-#     done
-# done
+    #### Generate answers ####
+    for guidance_strength in "${guidance_strength_lst[@]}"; do
+        for QUESTION_FILE in "${QUESTION_FILE_ls[@]}"; do
+            echo "Running $MODEL_VERSION inference with guidance_strength = $guidance_strength, seed = $SEED, batch_size = $BATCH_SIZE"
+            python ./marine/generate_${MODEL_NAME}.py \
+                --question_file $QUESTION_FILE \
+                --guidance_strength $guidance_strength \
+                --answer_path $OUTPUT_DIR \
+                --model_path $MODEL_VERSION \
+                --seed $SEED \
+                --batch_size $BATCH_SIZE \
+                --image_folder /home/ti514716/Data/coco/val2014 \
+                --temperature 0.6 \
+                --top_p 0.9 \
+                --max_new_tokens 64
+        done
+    done
+done
 
 benchmark_lst=(chair pope)
 for BENCHMARK in "${benchmark_lst[@]}"; do
